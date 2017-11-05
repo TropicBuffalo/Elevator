da7da5e3e5b0d1e136065442326509b1d9672fdc
TropicBuffalo <fannzio@gmail.com>
2017-11-05 15:13:24 +0000

    Elevator Broadcasting its floor number to the masses

--- elevator.c
+++ elevator.c
@@ -11,13 +11,13 @@ int main(int argc, char** argv) {
 	//For loop for elevator so it goes up and down
 	bool goUp = true;
 	int cnt = 0;
+	int root = 0;
+	int floorNr;
 	
 	MPI_Status Stat;
 	
-	//
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
 	//The elevator
 	//elevator[0] = current floor, elevator[1] = how many people inside
 	
@@ -29,64 +29,55 @@ int main(int argc, char** argv) {
 		for(int i = 0; i<12; i++) {
 			if(goUP) {
 				cnt++;
-				MPI_Bcast(cnt,1, MPI_INT, 0, MPI_COMM_WORLD);
 			}
 			else {
 				cnt--;
-				MPI_Bcast(cnt,1, MPI_INT, 0, MPI_COMM_WORLD);
 			}
 			printf("The elevator is on this floor: %d\n")
+			MPI_Bcast(cnt,1, MPI_INT, root, MPI_COMM_WORLD);
 			if(cnt == 3) {goUp = false;}
 			if(cnt == 0) {goUp = true;}
-		}
-		
-		
-		
-		for(;;){
-			//Get data from everyone and put it into an array, go down the array or find people on floor it stops on?
-			MPI_Recv(&people[i][0],3,MPI_INT,source,tag,MPI_COMM_WORLD);
-			
-			
-			
-			//Send out the guy out after searching the people array
-			MPI_Send(,MPI_COMM_WORLD);
 		}
 	}
 
 	//The people going into the elevator
 	//people[0] = Floor they are on, people[1] = Floor they are goign to, people[2] = if they are inside elevator
-	if (rank > 0) {
+	else {
 		int people[3] = {0,0,0};
 		int data[2];
 		source = 0;
 		tag = rank;
-		
-		//People sending their data to the elevator
-		MPI_Send(&people,3,MPI_INT,0, tag,MPI_COMM_WORLD);
-		
-		//While they are not in the elevator
-		while(people[2] == 0) {
-			//Listen for the elevator updates
-			MPI_Recv(&data[0], 2, MPI_INT, source,MPI_COMM_WORLD);
-			//If elevator is on same floor and less than 2 people onboard
-		 	if(people[0] == data[0] && data[1] < 2) {
-				people[2] = 1;
-			}
-		} 
-		
-		//While they are inside the elevator
-		while(people[2] == 1) {
-			//Listen for the elevator updates
-			MPI_Recv();
-			//Get out of the elevator if they have reached their floor
-			if(people[0] == people[1]) {
-				//Sleep command here or when the elevator sends the update?
-				//Exit the elevator
-				elevator[1]--;
-				//Change floors when people reach their destination
-				if (people[1] > 1) people[1]--;
-				else {
-					people[1]++;
+		//For loopt to mirrior the loop in the elevator
+		for(int i = 0; i<12; i++) {
+			//Receive which floor the elevator is on (It works I tested it)
+			MPI_Bcast(&floorNr,1,MPI_INT,root,MPI_COMM_WORLD);
+			//People sending their data to the elevator
+			//MPI_Send(&people,3,MPI_INT,0, tag,MPI_COMM_WORLD);
+			
+			//While they are not in the elevator
+			while(people[2] == 0) {
+				//Listen for the elevator updates
+				MPI_Recv(&data[0], 2, MPI_INT, source,MPI_COMM_WORLD);
+				//If elevator is on same floor and less than 2 people onboard
+				if(people[0] == data[0] && data[1] < 2) {
+					people[2] = 1;
+				}
+			} 
+			
+			//While they are inside the elevator
+			while(people[2] == 1) {
+				//Listen for the elevator updates
+				MPI_Recv();
+				//Get out of the elevator if they have reached their floor
+				if(people[0] == people[1]) {
+					//Sleep command here or when the elevator sends the update?
+					//Exit the elevator
+					elevator[1]--;
+					//Change floors when people reach their destination
+					if (people[1] > 1) people[1]--;
+					else {
+						people[1]++;
+					}
 				}
 			}
 		}
