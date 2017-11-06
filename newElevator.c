386bbb0864120600512c56ed2268f2b753e22f62
TropicBuffalo <fannzio@gmail.com>
2017-11-06 10:00:15 +0000

    .

--- newElevator.c
+++ newElevator.c
@@ -7,9 +7,10 @@
 int main(int argc, char** argv) {
 	int rank;
 	int root = 0;
+	
 	//Variables for elevator movement
 	bool goUp = true;
-	int cnt = 0;
+	int elevator[2] = {0};
 	int floorNr;
 	
 	//Variables for Gathering info about the people from the processors to the elevator
@@ -29,31 +30,15 @@ int main(int argc, char** argv) {
 	//people[2] = Floor they are going to, people[3] = If they are inside the elevator? Maybe not needed
 	if(rank > 0){
 		int people[4] = {rank,0,0,0};
-		
-		//For loop to mirror the loop in the elevator
+		r
 		for(int i = 0; i<12; i++) {
 			//Receive which floor the elevator is on
 			MPI_Bcast(&floorNr, 1, MPI_INT, root, MPI_COMM_WORLD);
 			//While they are not in the elevator
 			while(people[1] != people[2]) {
-				//If elevator is on same floor and less than 2 people onboard
-				if(people[0] == data[0] && data[1] < 2) {
-					people[2] = 1;
-				}
 			} 
 			//While they are inside the elevator
 			while(people[1] == people[2]) {
-				//Get out of the elevator if they have reached their floor
-				if(people[0] == people[1]) {
-					//Sleep command here or when the elevator sends the update?
-					//Exit the elevator
-					elevator[1]--;
-					//Change floors when people reach their destination
-					if (people[1] > 1) people[1]--;
-					else {
-						people[1]++;
-					}
-				}
 			}
 		}
 	}
@@ -61,6 +46,8 @@ int main(int argc, char** argv) {
 	//Gather the people array from all the processors into allPeople
 	MPI_Gather(&people[0], 4, MPI_INT, allPeople, 4, MPI_INT, 0, MPI_COMM_WORLD);
     
+	//The elevator
+	//elevator[0] = current floor, elevator[1] = how many people inside
 	if(rank == 0) {
 		//Copy the 1D allPeople into the 2D finalPeople to make things "easier", need to ignore the first row
 		memcpy(finalPeople, allPeople, 36* sizeof(int));
@@ -74,17 +61,17 @@ int main(int argc, char** argv) {
 		*/
 		for(int i =0;i < 12;i++) {
 			if(goUp) {
-				cnt++;
+				elevator[0]++;
 				sleep(2);
 			}
 			else{
-				cnt--;
+				elevator[0]--;
 				sleep(2);
 			}
-			printf("The elevator is on this floor: %d\n", cnt);
-			MPI_Bcast(&cnt,1,MPI_INT, root, MPI_COMM_WORLD);
-			if(cnt==3) {goUp =false;}
-			if(cnt==0) {goUp = true;}
+			printf("The elevator is on floor: %d\n", elevator[0]);
+			MPI_Bcast(&elevator[0],1,MPI_INT, root, MPI_COMM_WORLD);
+			if(elevator[0]==3) {goUp =false;}
+			if(elevator[0]==0) {goUp = true;}
 		}
     }
 	
